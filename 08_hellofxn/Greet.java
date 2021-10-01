7e04a006f6aa4da88997dabe235e83c12b7a5fcd
Corina Chen <66174591+cchen32@users.noreply.github.com>
2021-09-30 20:30:29 -0400

    Create Greet.java

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,25 @@
+/*Cheese -- Melody Lew and Corina Chen
+APCS
+HW8 -- Refactor Freshie Zero
+2021-09-30*/
+
+public class Greet{
+  public static void main(String[] args){
+    System.out.println("Why, hello there, " + greet("Moo") + ". How do you do?");
+    System.out.println("Why, hello there, " + greet("Foo") + ". How do you do?");
+    System.out.println("Why, hello there, " + greet("Doo") + ". How do you do?");
+  }
+  public static void greet(String){
+    System.out.println(String);
+  }
+}
+
+/* DISCOVERIES
+"void greet(String) is another function
+
+*/
+
+
+/*UNRESOLVED MYSTERIES
+What does it mean to have "Identifier expected"
+*/
