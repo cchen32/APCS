bee9bd585647de7d427af779b8a5cb3b7bb5ede0
Corina Chen <cchen32@Corinas-MacBook-Pro.local>
2021-10-03 10:32:54 -0400

    ???

---
+++ 09_bigsib/Greet.java
@@ -0,0 +1,12 @@
+//Team Never Advise Children: Corina Chen + Binktop
+//APCS
+//HW09: What Are BigSibs Good For?
+//2021-10-04
+
+public class Greet {
+	public static void main(String[] args){
+		BigSib.greet("Flim");
+		BigSib.greet("Flam");
+		BigSib.greet("Kazaam");
+	}
+}
