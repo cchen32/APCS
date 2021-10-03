113d2958226188e388d14eef9aaa623f33de3fb3
Corina Chen <cchen32@Corinas-MacBook-Pro.local>
2021-10-03 10:26:00 -0400

    First try

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,12 @@
+//Team Never Advise Chilren: Corina Chen + Binktop,...
+//APCS
+//HW09: What Are BigSibs Good For?
+//2021-10-04
+
+public class BigSib
+{
+	public BigSib(String name){
+		name = "Why, hello there, " + name + ". How do you do?";
+	}
+}
+
