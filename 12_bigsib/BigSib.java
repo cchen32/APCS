8fbaf2d567d5f8ac59488f9bbc1b5496650ba341
Corina Chen <66174591+cchen32@users.noreply.github.com>
2021-10-05 19:05:16 -0400

    Create BigSib.java

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,11 @@
+//Corina Chen
+//APCS
+//HW12: ON Elder Individuality and Elimination of Radio Fuzz
+//2021-10-05
+
+public class BigSib{
+  public String setHelloMsg;
+  public static String greet(String anInput){
+    return " " + anInput;
+  }
+}
