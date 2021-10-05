658afc51f944b8375c84f7d5a7749443eb6601e7
Corina Chen <66174591+cchen32@users.noreply.github.com>
2021-10-04 21:21:33 -0400

    Update BigSib.java

--- 10_bigsib/BigSib.java
+++ 10_bigsib/BigSib.java
@@ -7,6 +7,6 @@
 
 public class BigSib{
   public static String greet(String name){
-    System.out.println("Why, hello there, " + name + ". How do you do?");
+    return "Why, hello there, " + name + ". How do you do?";
   }
 }
