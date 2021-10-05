370557119bdd4b1ba6ad8ef1099d9e782a0105e3
Corina Chen <66174591+cchen32@users.noreply.github.com>
2021-10-04 21:22:56 -0400

    Update Greet.java

--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -4,7 +4,8 @@
 //2021-10-04
 
 /*Discoveries
-BigSib.java has no need for a main method because it's not the one being called. Also it's meant to be "String" as it BigSib.java returns String and not "void". */
+BigSib.java has no need for a main method because it's not the one being called. Also it's meant to be "String" as in BigSib.java returns String and not "void". Also I guess
+"return" is more practical than "System.out.println" as I had originally put it because that would be redundant.*/
 
 /*Unresolved mysteries
 What is static void?*/
