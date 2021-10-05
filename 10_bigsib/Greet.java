aad37fc067d72cd44aa17b01d53b0773b9fb7d2c
Corina Chen <66174591+cchen32@users.noreply.github.com>
2021-10-04 21:21:03 -0400

    Create Greet.java

---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,17 @@
+//Corina Chen + Binktop
+//APCS
+//HW10: Refactor Big Sib One
+//2021-10-04
+
+/*Discoveries
+BigSib.java has no need for a main method because it's not the one being called. Also it's meant to be "String" as it BigSib.java returns String and not "void". */
+
+/*Unresolved mysteries
+What is static void?*/
+
+public static Greet{
+  public static void main(String[] args)
+    System.out.println( BigSib.greet("Crosby") );
+    System.out.println( BigSib.greet("Stills") );
+    System.out.println( BigSib.greet("Nash") );
+}
