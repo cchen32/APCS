b4be9bb8811ab17f966388bfab0edd6a6ad74e42
Corina Chen <66174591+cchen32@users.noreply.github.com>
2021-10-04 21:15:12 -0400

    Create BigSib.java

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,12 @@
+//Corina Chen + Binktop
+//APCS
+//HW10: Refactor Big Sib One
+//2021-10-04
+
+//Discoveries and Unresolved Questions in Greet.java
+
+public class BigSib{
+  public static String greet(String name){
+    System.out.println("Why, hello there, " + name + ". How do you do?");
+  }
+}
