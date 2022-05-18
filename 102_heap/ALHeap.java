96a1f68a8a2e9736e1056273aa227f03e30a532a
Corina Chen <cchen32@stuy.edu>
2022-05-17 22:28:01 -0400

    S U S

--- 102_heap/ALHeap.java
+++ 102_heap/ALHeap.java
@@ -10,6 +10,17 @@
  * Implements a min heap using an ArrayList as underlying container
  */
 
+ /*
+ DISCOS:
+ 0. The equations to find parent/child position came into use.
+ 1. It is better to use while loop because we repeat the algo until a condition is false.
+
+ QCC:
+ 0. Chose to go simple with toString.
+ 1. Is it true in every case that if the leftChild is empty that the rightChild must also be empty?
+    That is if we go by the fact that each position must be filled up before going to the next leve.
+ */
+
 import java.util.ArrayList;
 
 public class ALHeap
@@ -34,9 +45,14 @@ public class ALHeap
    * a) a level-order traversal of the tree (simple version)
    * b) ASCII representation of the tree (more complicated, more fun)
    */
-  public String toString()
+  public String toString() // Choosing to go with simple
   {
-  }//O(?)
+    String retStr = "";
+    for (int i = 0; i < _heap.size(); i++){
+      retStr += _heap.get(i) + " ";
+    }
+    return retStr;
+  }//O(n) - Dependent on length of ArrayList
 
 
   /**
@@ -66,11 +82,25 @@ public class ALHeap
    * Inserts an element in the heap
    * Postcondition: Tree exhibits heap property.
    * ALGO:
-   * 0. Traverse the array to the very end
+   * 0. Append addVal to the end of the array.
+   * 1. Use the equation to find the index of its parent.
+   * 2. Compare addVal to the parent.
+   * 3. If addVal is greater than its parent, swap with parent.
+   * 4. Check with parent and grandparent, swap if necessary. Repeat until root if necessary.
    */
   public void add( Integer addVal )
   {
-  }//O(?)
+    _heap.add(addVal);
+    if (_heap.size() == 1){
+      return;
+    }
+    int pIndex = (_heap.indexOf(addVal) - 1) / 2;
+    while (minOf(addVal, _heap.get(pIndex)) == addVal){
+      int addValIndex = _heap.get(addVal);
+      swap(addValIndex, pIndex);
+      pIndex = (_heap.indexOf(addVal) - 1) / 2;
+    }
+  }//O(n) - Traversing through ArrayList once, dependent on length
 
 
   /**
@@ -78,22 +108,49 @@ public class ALHeap
    * Removes and returns least element in heap.
    * Postcondition: Tree maintains heap property.
    * ALGO:
-   * <your clear && concise procedure here>
+   * 0. Swap the element you want to remove with last element.
+   * 1. Remove last element
+   * 2. If there is another child, compare the child to shared parents. Swap as necessary.
+   * 3. Repeat Step 4 from add() until all is well.
    */
   public Integer removeMin()
   {
-  }//O(?)
+    if (isEmpty()) {
+      return null;
+    }
+    Integer ans = _heap.get(0);
+    swap(0, _heap.get(_heap.size() - 1));
+    _heap.set(_heap.size() - 1, null);
+    int index = 0;
+    while (minChildPos(index) != -1){
+      swap(index, _heap.indexOf(minChildPos(index)));
+      index = _heap.indexOf(minChildPos(index));
+    }
+    return ans;
+  }//O(n) - Traversing through ArrayList once, dependent on length
 
 
   /**
    * minChildPos(int)  ---  helper fxn for removeMin()
-   * Returns index of least child, orminChildPos
+   * Returns index of least child, or
    * -1 if no children, or if input pos is not in ArrayList
    * Postcondition: Tree unchanged
    */
   private int minChildPos( int pos )
   {
-  }//O(?)
+    int leftChild = 2 * pos + 1;
+    int rightChild = 2 * pos + 2;
+    if (_heap.get(pos) == null){
+      return -1;
+    }
+    else if (_heap.get(leftChild) == null) { // BC rightChild can't exist if leftChild doesn't exist
+      return -1;
+    }
+    else if (_heap.get(leftChild) > _heap.get(rightChild)) {
+      return rightChild;
+    }
+    return leftChild;
+  }//O(1) - Consistent despite ArrayList length,
 
 
   //~~~~~~~~~~~~~ aux helper fxns ~~~~~~~~~~~~~~
@@ -117,7 +174,6 @@ public class ALHeap
   //main method for testing
   public static void main( String[] args )
   {
-    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       ALHeap pile = new ALHeap();
 
       pile.add(2);
@@ -141,6 +197,7 @@ public class ALHeap
       pile.add(9);
       System.out.println(pile);
 
+      /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       System.out.println("removing " + pile.removeMin() + "...");
       System.out.println(pile);
       System.out.println("removing " + pile.removeMin() + "...");
